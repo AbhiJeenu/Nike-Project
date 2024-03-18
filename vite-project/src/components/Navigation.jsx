292810c688c4c0ac8b39fb162aec06bdd2a3cea1
AbhiJeenu <groverabhishek15@gmail.com>
2024-03-19 04:59:19 +0530

    Abhishek
    Nike-Project

---
+++ vite-project/src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+
+const Navigation = () =>{
+return(
+    <nav className="container">
+               <div className="logo">
+                <img src="/images/brand_logo.png" alt="logo" />
+               </div> 
+
+               <ul>
+                <li href="http://">Menu</li>
+                <li href="http://">Location</li>
+                <li href="http://">About</li>
+                <li href="http://">Contact</li>
+               </ul>
+            <button>Login</button>
+            </nav>
+ );
+}
+
+export default Navigation;
